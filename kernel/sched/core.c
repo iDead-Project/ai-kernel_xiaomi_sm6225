b2655be681c2edccc5c48da2149c277566bcdef2
darkhz <kmachanwenw@gmail.com>
2021-01-29 12:08:25 +0530

    sched/uclamp: Create a new uclamp.boosted tunable
    This complements the previous patch:
    sched/uclamp: Make uclamp_boosted() return proper boosted value
    
    We now can modify boost values for each taskgroup, just like in
    SchedTune, and uclamp_boosted() will correctly return the boost
    settings for the taskgroup.
    
    Change-Id: Ifa9b143edc621d3ef1d44088a6bcadce5ac2a5b0

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8703,6 +8703,27 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 }
 #endif /* CONFIG_UCLAMP_ASSIST */
 
+static int cpu_uclamp_boost_write_u64(struct cgroup_subsys_state *css,
+				   struct cftype *cftype, u64 boosted)
+{
+	struct task_group *tg;
+
+	if (boosted > 1)
+		return -EINVAL;
+	tg = css_tg(css);
+	tg->boosted = (unsigned int) boosted;
+
+	return 0;
+}
+
+static u64 cpu_uclamp_boost_read_u64(struct cgroup_subsys_state *css,
+				  struct cftype *cft)
+{
+	struct task_group *tg = css_tg(css);
+
+	return (u64) tg->boosted;
+}
+
 #endif /* CONFIG_UCLAMP_TASK_GROUP */
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
@@ -9068,6 +9089,12 @@ static struct cftype cpu_legacy_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_uclamp_ls_read_u64,
 		.write_u64 = cpu_uclamp_ls_write_u64,
+	},
+	{
+		.name = "uclamp.boosted",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.read_u64 = cpu_uclamp_boost_read_u64,
+		.write_u64 = cpu_uclamp_boost_write_u64,
 	},
 #endif
 	{ }	/* Terminate */
