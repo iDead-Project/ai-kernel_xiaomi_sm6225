2b7b9c4bcb949b4847155c6f5ac791165e8f61d1
Lorenz Bauer <lmb@cloudflare.com>
2022-08-01 15:29:15 +0800

    UPSTREAM: bpf: Add PROG_TEST_RUN support for sk_lookup programs
    commit 7c32e8f8bc33a5f4b113a630857e46634e3e143b upstream.
    
    Allow to pass sk_lookup programs to PROG_TEST_RUN. User space
    provides the full bpf_sk_lookup struct as context. Since the
    context includes a socket pointer that can't be exposed
    to user space we define that PROG_TEST_RUN returns the cookie
    of the selected socket or zero in place of the socket pointer.
    
    We don't support testing programs that select a reuseport socket,
    since this would mean running another (unrelated) BPF program
    from the sk_lookup test handler.
    
    Change-Id: I7af748e3f11804e4e1ad0c532685f0c3dfaf4816
    Signed-off-by: Lorenz Bauer <lmb@cloudflare.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20210303101816.36774-3-lmb@cloudflare.com
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1444,6 +1444,9 @@ int bpf_prog_test_run_flow_dissector(struct bpf_prog *prog,
 int bpf_prog_test_run_raw_tp(struct bpf_prog *prog,
 			     const union bpf_attr *kattr,
 			     union bpf_attr __user *uattr);
+int bpf_prog_test_run_sk_lookup(struct bpf_prog *prog,
+				const union bpf_attr *kattr,
+				union bpf_attr __user *uattr);
 bool btf_ctx_access(int off, int size, enum bpf_access_type type,
 		    const struct bpf_prog *prog,
 		    struct bpf_insn_access_aux *info);
@@ -1657,9 +1660,11 @@ static inline int bpf_prog_test_run_flow_dissector(struct bpf_prog *prog,
 	return -ENOTSUPP;
 }
 
-static inline bool unprivileged_ebpf_enabled(void)
+static inline int bpf_prog_test_run_sk_lookup(struct bpf_prog *prog,
+					      const union bpf_attr *kattr,
+					      union bpf_attr __user *uattr)
 {
-	return false;
+	return -ENOTSUPP;
 }
 
 static inline void bpf_map_put(struct bpf_map *map)
@@ -1675,6 +1680,11 @@ static inline const struct bpf_func_proto *
 bpf_base_func_proto(enum bpf_func_id func_id)
 {
 	return NULL;
+}
+
+static inline bool unprivileged_ebpf_enabled(void)
+{
+	return false;
 }
 #endif /* CONFIG_BPF_SYSCALL */
 
