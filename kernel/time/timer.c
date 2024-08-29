7af20abf383f118e03c7625985db57742f706e5c
Anna-Maria Behnsen <anna-maria@linutronix.de>
2024-08-29 09:41:33 +0200

    timers: Remove historical extra jiffie for timeout in msleep()
    msleep() and msleep_interruptible() add a jiffie to the requested timeout.
    
    This extra jiffie was introduced to ensure that the timeout will not happen
    earlier than specified.
    
    Since the rework of the timer wheel, the enqueue path already takes care of
    this. So the extra jiffie added by msleep*() is pointless now.
    
    Remove this extra jiffie in msleep() and msleep_interruptible().
    
    Change-Id: I706cf4ad48ca197045308279a9f927963ef4382c
    Signed-off-by: Anna-Maria Behnsen <anna-maria@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://lore.kernel.org/all/20240829074133.4547-1-anna-maria@linutronix.de
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -2093,7 +2093,7 @@ void __init init_timers(void)
  */
 void msleep(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs_to_jiffies(msecs);
 
 	while (timeout)
 		timeout = schedule_timeout_uninterruptible(timeout);
@@ -2107,7 +2107,7 @@ EXPORT_SYMBOL(msleep);
  */
 unsigned long msleep_interruptible(unsigned int msecs)
 {
-	unsigned long timeout = msecs_to_jiffies(msecs) + 1;
+	unsigned long timeout = msecs_to_jiffies(msecs);
 
 	while (timeout && !signal_pending(current))
 		timeout = schedule_timeout_interruptible(timeout);
