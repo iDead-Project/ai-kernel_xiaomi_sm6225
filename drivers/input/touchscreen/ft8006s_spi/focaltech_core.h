4b8e5275febbd002753b04907cc65b122fd04868
iDead XD <reinforced113@gmail.com>
2025-06-02 16:23:59 +0000

    drivers: input: ft8006s_spi: Implement double tap attr

--- drivers/input/touchscreen/ft8006s_spi/focaltech_core.h
+++ drivers/input/touchscreen/ft8006s_spi/focaltech_core.h
@@ -219,6 +219,7 @@ struct fts_ts_data {
     struct pinctrl_state *pins_release;
 #endif
     struct notifier_block drm_notif;
+    int double_tap_pressed;
 
 	struct mutex reg_lock;
 	struct device *fts_touch_dev;
