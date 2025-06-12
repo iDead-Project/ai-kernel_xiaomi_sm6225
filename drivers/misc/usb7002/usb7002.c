0150837a30af9f7c7550c22a111746917b0a2aeb
Rafi Ahmad Firdaus <rafifirdaus12bb@gmail.com>
2025-06-12 03:10:06 +0000

    Merge tag 'LA.UM.9.15.2.c1-15400-KAMORTA.QSSI14.0' of https://git.codelinaro.org/clo/la/kernel/msm-4.19 into very-personal
    LA.UM.9.15.2.c1-15400-KAMORTA.QSSI14.0

--- drivers/misc/usb7002/usb7002.c
+++ drivers/misc/usb7002/usb7002.c
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -9,11 +9,41 @@
 #include <linux/of.h>
 #include <linux/sysfs.h>
 #include <linux/slab.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
+#include <linux/regulator/consumer.h>
+
+#define USB7002_GPIO_STATUS_HIGH    1
+#define USB7002_GPIO_STATUS_LOW     0
+#define MAX_PROPS_SIZE              32
 
 struct usb7002_device {
 	struct i2c_client *client;
 	struct device *dev;
 };
+
+struct usb7002_reg_data {
+	/* Voltage regulator handle */
+	struct regulator *reg;
+
+	/* Regulator name */
+	const char *name;
+
+	/* Voltage level to be set */
+	u32 low_vol_level;
+	u32 high_vol_level;
+
+	/* Load values for low power and high power mode */
+	u32 lpm_uA;
+	u32 hpm_uA;
+
+	/* Is this regulator needs to be always on? */
+	bool is_always_on;
+
+	/* Is low power mode setting required for this regulator? */
+	bool lpm_sup;
+};
+
 int usb7002_value;
 struct usb7002_device *u7002 = NULL;
 
@@ -308,6 +338,128 @@ i2c_fail:
 }
 EXPORT_SYMBOL(usb7002_switch_host);
 
+static int enable_ldo4_regulator(struct device *dev)
+{
+	int len, ret = 0;
+	const __be32 *prop;
+	char prop_name[MAX_PROPS_SIZE];
+	struct usb7002_reg_data *vreg;
+	struct device_node *np = dev->of_node;
+	char *vreg_name = "vdd-io";
+
+	snprintf(prop_name, MAX_PROPS_SIZE, "vdd-io-supply");
+	if (!of_parse_phandle(np, prop_name, 0)) {
+		dev_err(dev, "No vreg data found for vdd-io");
+		return ret;
+	}
+
+	vreg = devm_kzalloc(dev, sizeof(*vreg), GFP_KERNEL);
+	if (!vreg) {
+		ret = -ENOMEM;
+		return ret;
+	}
+
+	vreg->name = vreg_name;
+	snprintf(prop_name, MAX_PROPS_SIZE, "qcom,%s-always-on", vreg_name);
+
+	if (of_get_property(np, prop_name, NULL))
+		vreg->is_always_on = true;
+
+	snprintf(prop_name, MAX_PROPS_SIZE, "qcom,%s-lpm-sup", vreg_name);
+	if (of_get_property(np, prop_name, NULL))
+		vreg->lpm_sup = true;
+
+	snprintf(prop_name, MAX_PROPS_SIZE, "qcom,%s-voltage-level", vreg_name);
+	prop = of_get_property(np, prop_name, &len);
+	if (!prop || (len != (2 * sizeof(__be32)))) {
+		dev_warn(dev, "%s %s property\n", prop ?
+				"invalid format" : "no", prop_name);
+	} else {
+		vreg->low_vol_level = be32_to_cpup(&prop[0]);
+		vreg->high_vol_level = be32_to_cpup(&prop[1]);
+	}
+
+	snprintf(prop_name, MAX_PROPS_SIZE, "qcom,%s-current-level", vreg_name);
+	prop = of_get_property(np, prop_name, &len);
+	if (!prop || (len != (2 * sizeof(__be32)))) {
+		pr_err("%s: invalid property vdd-io", __func__);
+	} else {
+		vreg->lpm_uA = be32_to_cpup(&prop[0]);
+		vreg->hpm_uA = be32_to_cpup(&prop[1]);
+	}
+
+	pr_debug("%s: %s: %s %s vol=[%d %d]uV, curr=[%d %d]uA\n", __func__,
+			vreg->name, vreg->is_always_on ? "always_on," : "",
+			vreg->lpm_sup ? "lpm_sup," : "", vreg->low_vol_level,
+			vreg->high_vol_level, vreg->lpm_uA, vreg->hpm_uA);
+
+	vreg->reg = devm_regulator_get(dev, vreg->name);
+	if (IS_ERR(vreg->reg)) {
+		ret = PTR_ERR(vreg->reg);
+		pr_err("%s: devm_regulator_get(%s) failed. ret=%d\n",
+				__func__, vreg->name, ret);
+		return ret;
+	}
+
+	ret = regulator_set_voltage(vreg->reg, vreg->low_vol_level,
+			vreg->high_vol_level);
+	if (ret) {
+		pr_err("%s: regulator_set_voltage(%s)failed. ret=%d\n",
+				__func__, vreg->name, ret);
+		return ret;
+	}
+
+	ret = regulator_enable(vreg->reg);
+	if (ret) {
+		pr_err("%s: regulator_enable(%s) failed. ret=%d\n",
+				__func__, vreg->name, ret);
+		return ret;
+	}
+
+	pr_debug("%s: regulator enabled", __func__);
+	return ret;
+}
+
+static int usb7002_reset(struct device *dev)
+{
+	uint32_t usb7002_reset_gpio;
+	int ret = 0;
+	struct device_node *dev_node;
+
+	dev_node = dev->of_node;
+	if (!dev_node) {
+		ret = -ENODEV;
+		goto exit;
+	}
+
+	usb7002_reset_gpio = of_get_named_gpio(dev_node, "reset-gpios", 0);
+	if (usb7002_reset_gpio < 0) {
+		ret = -ENOENT;
+		goto exit;
+	}
+
+	if (!gpio_is_valid(usb7002_reset_gpio)) {
+                pr_err("%s: usb7002_reset_gpio (%d) is not valid\n",
+                                __func__, usb7002_reset_gpio);
+                ret = -EINVAL;
+                goto exit;
+        }
+
+        if (gpio_direction_output(usb7002_reset_gpio, USB7002_GPIO_STATUS_LOW)) {
+                pr_err("%s: Setting usb7002_reset_gpio (%d) direction to output mode failed\n",
+                                __func__, usb7002_reset_gpio);
+                ret = -EIO;
+                goto exit;
+        }
+
+        gpio_set_value(usb7002_reset_gpio, USB7002_GPIO_STATUS_HIGH);
+	pr_debug("%s: usb7002_reset success\n", __func__);
+exit:
+        return ret;
+}
+
+
+
 static int usb7002_i2c_probe(struct i2c_client *client,
 		const struct i2c_device_id *id) {
 
@@ -323,11 +475,19 @@ static int usb7002_i2c_probe(struct i2c_client *client,
 	u7002->dev = &client->dev;
 	i2c_set_clientdata(client, u7002);
 
+	ret = usb7002_reset(u7002->dev);
+	if (ret != 0) {
+		pr_err("%s: usb7002_reset failed", __func__);
+		goto free_mem;
+	}
+
 	ret = usb7002_switch_peripheral();
 	if (ret < 0) {
 		pr_err("%s flex failed\n", __func__);
 		goto free_mem;
 	}
+
+	enable_ldo4_regulator(u7002->dev);
 	pr_info("%s success\n", __func__);
 	return 0;
 
--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -498,6 +498,14 @@ static int pm8xxx_rtc_freeze(struct device *dev)
 
 	devm_free_irq(rtc_dd->rtc_dev, rtc_dd->rtc_alarm_irq, rtc_dd);
 
+	/*
+	 * Added wake up capability logic for RTC alarm based on DTS entry.
+	 */
+	if (of_property_read_bool(pdev->dev.of_node, "qcom,disable-alarm-wakeup")) {
+		device_set_wakeup_capable(&pdev->dev, false);
+	}
+
+	dev_dbg(&pdev->dev, "Probe success RTC !!\n");
 	return 0;
 }
 
