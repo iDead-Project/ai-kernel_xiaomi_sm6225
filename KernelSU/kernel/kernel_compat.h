90b18a5e8a4d9a17953d9d1c334700aebbdb539e
iDeadXS <datarafi43@gmail.com>
2025-06-21 19:18:56 +0000

    KernelSU: import 12103-susfs155 debloated
    Imported from https://github.com/backslashxx/KernelSU

--- KernelSU/kernel/kernel_compat.h
+++ KernelSU/kernel/kernel_compat.h
@@ -15,12 +15,26 @@ extern struct key *init_session_keyring;
 #endif
 
 extern void ksu_android_ns_fs_check();
-extern int ksu_access_ok(const void *addr, unsigned long size);
 extern struct file *ksu_filp_open_compat(const char *filename, int flags,
 					 umode_t mode);
 extern ssize_t ksu_kernel_read_compat(struct file *p, void *buf, size_t count,
 				      loff_t *pos);
 extern ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
 				       size_t count, loff_t *pos);
+
+extern int ksu_access_ok(const void *addr, unsigned long size);
+extern long ksu_copy_from_user_nofault(void *dst, const void __user *src, size_t size);
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 11, 0) && !defined(KSU_HAS_ITERATE_DIR)
+struct dir_context {
+	const filldir_t actor;
+	loff_t pos;
+};
+
+static int iterate_dir(struct file *file, struct dir_context *ctx)
+{
+	return vfs_readdir(file, ctx->actor, ctx);
+}
+#endif // KSU_HAS_ITERATE_DIR
 
 #endif
--- KernelSU/kernel/ksu.c
+++ KernelSU/kernel/ksu.c
@@ -2,20 +2,17 @@
 #include <linux/fs.h>
 #include <linux/kobject.h>
 #include <linux/module.h>
+#include <generated/utsrelease.h>
+#include <generated/compile.h>
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION macros */
 #include <linux/workqueue.h>
 #include <linux/init.h>
 
 #include "allowlist.h"
-#include "arch.h"
 #include "core_hook.h"
 #include "klog.h" // IWYU pragma: keep
 #include "ksu.h"
 #include "throne_tracker.h"
-
-#ifdef CONFIG_KSU_SUSFS
-#include <linux/susfs.h>
-#endif
 
 unsigned int enable_ksu = 1;
 
@@ -40,6 +37,7 @@ bool ksu_queue_work(struct work_struct *work)
 	return queue_work(ksu_workqueue, work);
 }
 
+#ifdef KSU_USE_STRUCT_FILENAME
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 					void *argv, void *envp, int *flags);
 
@@ -53,13 +51,77 @@ int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 	return ksu_handle_execveat_sucompat(fd, filename_ptr, argv, envp,
 					    flags);
 }
+#endif // KSU_USE_STRUCT_FILENAME
 
-int __init ksu_kernelsu_init(void)
+// track backports and other quirks here
+// ref: kernel_compat.c, Makefile
+// yes looks nasty
+#ifdef KSU_USE_STRUCT_FILENAME
+	#define FEAT_1 " +uses_struct_filename"
+#else
+	#define FEAT_1 ""
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0) && !defined(CONFIG_KSU_LSM_SECURITY_HOOKS)
+	#define FEAT_2 " -lsm_hooks"
+#else
+	#define FEAT_2 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)) && defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
+	#define FEAT_3 " +allowlist_workaround"
+#else
+	#define FEAT_3 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0)) && defined(KSU_HAS_MODERN_EXT4)
+	#define FEAT_4 " +ext4_unregister_sysfs"
+#else
+	#define FEAT_4 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)) && defined(KSU_HAS_PATH_UMOUNT)
+	#define FEAT_5 " +path_umount"
+#else
+	#define FEAT_5 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)) && defined(KSU_STRNCPY_FROM_USER_NOFAULT)
+	#define FEAT_6 " +strncpy_from_user_nofault"
+#else
+	#define FEAT_6 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 3, 0)) && defined(KSU_STRNCPY_FROM_UNSAFE_USER)
+	#define FEAT_7 " +strncpy_from_unsafe_user"
+#else
+	#define FEAT_7 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_READ)
+	#define FEAT_8 " +new_kernel_read"
+#else
+	#define FEAT_8 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) && defined(KSU_NEW_KERNEL_WRITE)
+	#define FEAT_9 " +new_kernel_write"
+#else
+	#define FEAT_9 ""
+#endif
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(3, 16, 0)) && defined(KSU_HAS_FOP_READ_ITER)
+	#define FEAT_10 " +read_iter"
+#else
+	#define FEAT_10 ""
+#endif
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 11, 0) && defined(KSU_HAS_ITERATE_DIR)
+	#define FEAT_11 " +iterate_dir"
+#else
+	#define FEAT_11 ""
+#endif
+
+#define EXTRA_FEATURES FEAT_1 FEAT_2 FEAT_3 FEAT_4 FEAT_5 FEAT_6 FEAT_7 FEAT_8 FEAT_9 FEAT_10 FEAT_11
+
+int __init kernelsu_init(void)
 {
 	if (enable_ksu < 1) {
 		pr_info_once(" is disabled");
 		return 0;
 	}
+
+	pr_info("Initialized on: %s (%s) with ksuver: %s%s\n", UTS_RELEASE, UTS_MACHINE, __stringify(KSU_VERSION), EXTRA_FEATURES);
 
 #ifdef CONFIG_KSU_DEBUG
 	pr_alert("*************************************************************");
@@ -69,10 +131,6 @@ int __init ksu_kernelsu_init(void)
 	pr_alert("**                                                         **");
 	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
 	pr_alert("*************************************************************");
-#endif
-
-#ifdef CONFIG_KSU_SUSFS
-	susfs_init();
 #endif
 
 	ksu_core_init();
@@ -86,11 +144,11 @@ int __init ksu_kernelsu_init(void)
 	return 0;
 }
 
-void ksu_kernelsu_exit(void)
+void kernelsu_exit(void)
 {
 	if (enable_ksu < 1)
 		return;
-
+	
 	ksu_allowlist_exit();
 
 	ksu_throne_tracker_exit();
@@ -99,8 +157,8 @@ void ksu_kernelsu_exit(void)
 
 }
 
-module_init(ksu_kernelsu_init);
-module_exit(ksu_kernelsu_exit);
+module_init(kernelsu_init);
+module_exit(kernelsu_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("weishu");
