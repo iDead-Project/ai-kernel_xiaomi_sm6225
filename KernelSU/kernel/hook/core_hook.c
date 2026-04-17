f4ec0971d0c135fa3848cc200155bf3adf3dd0a5
iDeadXS <datarafi43@gmail.com>
2026-04-17 10:49:02 +0700

    KernelSU: Import xxKSU master

---
+++ KernelSU/kernel/hook/core_hook.c
@@ -0,0 +1,444 @@
+#ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
+#define LSM_HANDLER_TYPE static int
+#else
+#define LSM_HANDLER_TYPE int
+#endif
+
+LSM_HANDLER_TYPE ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry)
+{
+	if (!current->mm) {
+		// skip kernel threads
+		return 0;
+	}
+
+	if (current_uid().val != 1000) {
+		// skip non system uid
+		return 0;
+	}
+
+	if (!old_dentry || !new_dentry) {
+		return 0;
+	}
+
+	// /data/system/packages.list.tmp -> /data/system/packages.list
+	if (strcmp(new_dentry->d_iname, "packages.list")) {
+		return 0;
+	}
+
+	char path[128];
+	char *buf = dentry_path_raw(new_dentry, path, sizeof(path));
+	if (IS_ERR(buf)) {
+		pr_err("dentry_path_raw failed.\n");
+		return 0;
+	}
+
+	if (!strstr(buf, "/system/packages.list")) {
+		return 0;
+	}
+	pr_info("renameat: %s -> %s, new path: %s\n", old_dentry->d_iname,
+		new_dentry->d_iname, buf);
+
+	track_throne(false);
+
+	return 0;
+}
+
+LSM_HANDLER_TYPE ksu_handle_setuid(struct cred *new, const struct cred *old)
+{
+	if (!new || !old) {
+		return 0;
+	}
+
+	uid_t new_uid = ksu_get_uid_t(new->uid);
+	uid_t old_uid = ksu_get_uid_t(old->uid);
+
+	// old process is not root, ignore it.
+	if (0 != old_uid)
+		return 0;
+
+	// we dont have those new fancy things upstream has
+	// lets just do original thing where we disable seccomp
+	if (unlikely(is_uid_manager(new_uid))) {
+		disable_seccomp();
+		pr_info("install fd for: %d\n", new_uid);
+		ksu_install_fd();
+		return 0;
+	}
+
+	if (unlikely(ksu_is_allow_uid_for_current(new_uid))) {
+		disable_seccomp();
+		return 0;
+	}
+
+	return ksu_handle_umount(new, old);
+}
+
+LSM_HANDLER_TYPE ksu_bprm_check(struct linux_binprm *bprm)
+{
+#ifdef CONFIG_KSU_FEATURE_SULOG
+	if (unlikely(!current->seccomp.mode))
+		ksu_sulog_emit_bprm((const char *)bprm->filename);
+#endif
+
+	if (likely(!ksu_execveat_hook))
+		return 0;
+
+	ksu_grab_init_session_keyring((const char *)bprm->filename);
+
+	ksu_handle_pre_ksud((const char *)bprm->filename);
+
+	return 0;
+}
+
+LSM_HANDLER_TYPE ksu_file_permission(struct file *file, int mask)
+{
+	if (likely(!ksu_vfs_read_hook))
+		return 0;
+
+	ksu_install_rc_hook(file);
+
+	return 0;
+}
+
+#ifdef CONFIG_KSU_LSM_SECURITY_HOOKS
+static int ksu_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
+			    struct inode *new_inode, struct dentry *new_dentry)
+{
+	return ksu_handle_rename(old_dentry, new_dentry);
+}
+
+static int ksu_task_fix_setuid(struct cred *new, const struct cred *old,
+			       int flags)
+{
+	return ksu_handle_setuid(new, old);
+}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0)
+static struct security_hook_list ksu_hooks[] = {
+	LSM_HOOK_INIT(inode_rename, ksu_inode_rename),
+	LSM_HOOK_INIT(task_fix_setuid, ksu_task_fix_setuid),
+	LSM_HOOK_INIT(bprm_check_security, ksu_bprm_check),
+#if !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) && !defined(CONFIG_KSU_KPROBES_KSUD)
+	LSM_HOOK_INIT(file_permission, ksu_file_permission),
+#endif
+};
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+static void ksu_lsm_hook_init(void)
+{
+	security_add_hooks(ksu_hooks, ARRAY_SIZE(ksu_hooks), "ksu");
+}
+
+#else
+static void ksu_lsm_hook_init(void)
+{
+	security_add_hooks(ksu_hooks, ARRAY_SIZE(ksu_hooks));
+}
+#endif //  < 4.11
+
+#else // 4.2
+
+// selinux_ops (LSM), security_operations struct tampering for ultra legacy
+
+extern long copy_from_kernel_nofault(void *dst, const void *src, size_t size);
+static uintptr_t selinux_ops_addr = NULL;
+
+static int (*orig_inode_rename) (struct inode *old_dir, struct dentry *old_dentry,
+			     struct inode *new_dir, struct dentry *new_dentry) = NULL;
+static int hook_inode_rename(struct inode *old_inode, struct dentry *old_dentry,
+			    struct inode *new_inode, struct dentry *new_dentry)
+{
+	ksu_inode_rename(old_inode, old_dentry, new_inode, new_dentry);
+	return orig_inode_rename(old_inode, old_dentry, new_inode, new_dentry);
+}
+
+static int (*orig_task_fix_setuid) (struct cred *new, const struct cred *old, int flags) = NULL;
+static int hook_task_fix_setuid(struct cred *new, const struct cred *old, int flags)
+{
+	ksu_task_fix_setuid(new, old, flags);
+	return orig_task_fix_setuid(new, old, flags);
+}
+
+static int (*orig_bprm_check_security)(struct linux_binprm *bprm) = NULL;
+static int hook_bprm_check_security(struct linux_binprm *bprm)
+{
+	ksu_bprm_check(bprm);
+	return orig_bprm_check_security(bprm);
+}
+
+static int (*orig_file_permission) (struct file *file, int mask) = NULL;
+static int hook_file_permission(struct file *file, int mask)
+{
+
+	ksu_file_permission(file, mask);
+	return orig_file_permission(file, mask);
+}
+
+static inline bool verify_selinux_cred_free(void *fn_ptr)
+{
+	bool success = false;
+
+	if (!fn_ptr)
+		return false;
+
+	// this is not good, calling random function pointers.
+	// but straight up pointer-swapping is worse.
+	// TODO: better way to verify
+	// ref: https://elixir.bootlin.com/linux/v3.18.140/source/security/selinux/hooks.c#L3474
+	void (*selinux_cred_free_fn)(struct cred *) = fn_ptr;
+
+	struct cred dummy_cred;
+
+	// explicitly set it to NULL
+	// make sure this happens!
+	// #1. it wont trigger BUG_ON
+	// #2. this way it will kfree(NULL), which does nothing
+	*(volatile void **)&dummy_cred.security = NULL;
+	barrier();
+
+	selinux_cred_free_fn(&dummy_cred);
+
+	// check if selinux_cred_free is successful
+	if ((unsigned long)*(volatile void **)&dummy_cred.security == 0x7UL)
+		success = true;
+
+	pr_info("selinux_cred_free: 0x%lx cred->security: 0x%lx success: %d\n", (unsigned long)fn_ptr, (unsigned long)dummy_cred.security, success);
+
+	return success;
+}
+
+static noinline bool check_candidate(uintptr_t addr)
+{
+	struct security_operations *candidate = (struct security_operations *)addr;
+
+	char char_buf[sizeof("selinux")];
+	if (copy_from_kernel_nofault(char_buf, (void *)addr, sizeof("selinux") ))
+		return false;
+
+	if (!!strcmp(char_buf, "selinux"))
+		return false;
+
+	// candidate found!
+	pr_info("%s: candidate selinux_ops at 0x%lx\n", __func__, (long)addr);
+
+	uintptr_t cred_free_fn_ptr;
+	if (copy_from_kernel_nofault(&cred_free_fn_ptr, &candidate->cred_free, sizeof(void *)))
+		return false;
+
+	// "probably useless but wont hurt" verify, the fn_ptr should be inside stext to etext range
+	extern char _stext[], _etext[];
+	if (cred_free_fn_ptr < (uintptr_t)_stext || cred_free_fn_ptr > (uintptr_t)_etext)
+		return false;
+
+	pr_info("%s: candidate selinux_cred_free at 0x%lx\n", __func__, (long)cred_free_fn_ptr);
+
+	return verify_selinux_cred_free((void *)cred_free_fn_ptr);
+}
+
+/** 
+ * we do this in blocks of sequential 10k pointers.
+ * 10k pointers up, 10k pointers down
+ * this is predictable, more cache friendly, no trashing.
+ *
+ * one up, one down oscillating scan isn't as friendly to teh cahce.
+ * once ptrdiff of up vs down is larger than L1, it will be trashy.
+ *
+ */
+static noinline void *hunt_for_selinux_ops(void *heuristic_ptr)
+{
+	uintptr_t anchor = (uintptr_t)heuristic_ptr;
+	uintptr_t curr;
+	unsigned long iter_count = 0;
+	unsigned long max_index = 10000; // max number of pointers to test, one way
+	unsigned long i = 0;
+
+	uintptr_t start = anchor - max_index * sizeof(void *);
+	uintptr_t end = anchor + max_index * sizeof(void *);
+	pr_info("%s: scan range: 0x%lx - 0x%lx anchor: 0x%lx\n", __func__, (long)start, (long)end, (long)anchor);
+
+scan_up:
+	if (i >= max_index) {
+		i = 1;
+		goto scan_down;
+	}
+
+	curr = anchor + (i * sizeof(void *));
+	i++;
+	iter_count++;
+
+	if (check_candidate(curr))
+		goto found;
+
+	goto scan_up;
+
+scan_down:
+	if (i >= max_index)
+		goto not_found;
+
+	curr = anchor - (i * sizeof(void *));
+	i++;
+	iter_count++;
+
+	if (check_candidate(curr))
+		goto found;
+
+	goto scan_down;
+
+found:
+	pr_info("%s: found selinux_ops at 0x%lx iter_count: %lu \n", __func__, curr, iter_count);
+	return (void *)curr;
+
+not_found:
+	pr_info("%s: selinux_ops not found in range! iter_count: %lu \n", __func__, iter_count);
+	return NULL;
+}
+
+static inline void set_selinux_ops()
+{
+	extern int selinux_enabled;
+	extern struct security_class_mapping secclass_map[];
+	extern struct list_head crypto_alg_list;
+	extern unsigned int avc_cache_threshold;
+	
+	struct security_operations *ops = NULL;
+
+// if user exports selinux_ops, we just go for it!
+#ifdef KSU_HAS_EXPORTED_SELINUX_OPS
+	extern struct security_operations selinux_ops;
+	if (!ops)
+		ops = (struct security_operations *)&selinux_ops;
+#endif
+
+// not always available, can also fail, but it wont hurt to try.
+#ifdef CONFIG_KALLSYMS
+	if (!ops)
+		ops = (struct security_operations *)kallsyms_lookup_name("selinux_ops");
+#endif
+
+#ifdef CONFIG_KEYS
+	extern struct key_user root_key_user;
+	if (!ops)
+		ops = (struct security_operations *)hunt_for_selinux_ops((void *)&root_key_user);
+#endif
+
+	if (!ops)
+		ops = (struct security_operations *)hunt_for_selinux_ops((void *)&avc_cache_threshold);
+
+	if (!ops)
+		ops = (struct security_operations *)hunt_for_selinux_ops((void *)&crypto_alg_list);
+
+	if (!ops)
+		ops = (struct security_operations *)hunt_for_selinux_ops((void *)&selinux_enabled);
+
+	if (!ops)
+		ops = (struct security_operations *)hunt_for_selinux_ops((void *)&secclass_map);
+
+	if (!ops)
+		return;
+
+	selinux_ops_addr = (uintptr_t)ops;	
+}
+
+static void ksu_lsm_hook_restore(void)
+{
+	struct security_operations *ops = (struct security_operations *)selinux_ops_addr;
+
+	if (!ops)
+		return;
+
+	if (!!strcmp((char *)ops, "selinux"))
+		return;
+
+	pr_info("%s: selinux_ops: 0x%lx .name = %s\n", __func__, (long)ops, (const char *)ops );
+
+	preempt_disable();
+	local_irq_disable();
+
+#ifndef CONFIG_KSU_FEATURE_SULOG
+	if (orig_bprm_check_security) {
+		pr_info("%s: restoring: 0x%lx to 0x%lx\n", __func__, (long)ops->bprm_check_security, (long)orig_bprm_check_security);
+		ops->bprm_check_security = orig_bprm_check_security;
+	}
+#endif
+
+	if (orig_file_permission) {
+		pr_info("%s: restoring: 0x%lx to 0x%lx\n", __func__, (long)ops->file_permission, (long)orig_file_permission);
+		ops->file_permission = orig_file_permission;
+	}
+
+	smp_mb();
+
+	local_irq_enable();
+	preempt_enable();
+	
+	return;
+}
+
+static int execveat_hook_wait_fn(void *data)
+{
+loop_start:
+
+	msleep(1000);
+
+	if (*(volatile bool *)&ksu_execveat_hook)
+		goto loop_start;
+
+	ksu_lsm_hook_restore();
+
+	return 0;
+}
+
+static void execveat_hook_wait_thread()
+{
+	kthread_run(execveat_hook_wait_fn, NULL, "unhook");
+}
+
+static void ksu_lsm_hook_init(void)
+{
+	set_selinux_ops();
+
+	struct security_operations *ops = (struct security_operations *)selinux_ops_addr;
+	if (!ops)
+		return;
+
+	if (!!strcmp((char *)ops, "selinux"))
+		return;
+
+	pr_info("%s: selinux_ops: 0x%lx .name = %s\n", __func__, (long)ops, (const char *)ops );
+
+	preempt_disable();
+	local_irq_disable();
+
+	orig_inode_rename = ops->inode_rename;
+	ops->inode_rename = hook_inode_rename;
+
+	orig_task_fix_setuid = ops->task_fix_setuid;
+	ops->task_fix_setuid = hook_task_fix_setuid;
+
+	orig_bprm_check_security = ops->bprm_check_security;
+	ops->bprm_check_security = hook_bprm_check_security;
+
+#if !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) && !defined(CONFIG_KSU_KPROBES_KSUD)
+	orig_file_permission = ops->file_permission;
+	ops->file_permission = hook_file_permission;
+#endif
+
+	smp_mb();
+
+	local_irq_enable();
+	preempt_enable();
+	
+	execveat_hook_wait_thread();
+	return;
+}
+
+#endif // < 4.2
+
+#else
+void __init ksu_lsm_hook_init(void) { } // nothing, no-op
+#endif // CONFIG_KSU_LSM_SECURITY_HOOKS
+
+void __init ksu_core_init(void)
+{
+	ksu_lsm_hook_init();
+}
